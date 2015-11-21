32c65414f0200a3c3aa4accebde4b76cbc608f2d
Armando <apiconz@gmail.com>
2015-11-21 18:08:27 -0500

    Configuraciones iniciales del proyecto

---
+++ app/src/main/java/pe/boli/android/swlima_boli/application/BoliApp.java
@@ -0,0 +1,14 @@
+package pe.boli.android.swlima_boli.application;
+
+import android.app.Application;
+
+/**
+ * Created by Armando on 11/21/2015.
+ */
+public class BoliApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
